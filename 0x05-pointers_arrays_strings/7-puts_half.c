588b4abdfb1602eeb3b1abe58470df5a2ea97774
Scratche <Pharmflonimo@gmail.com>
2023-02-28 11:34:44 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,16 @@
 #include 'main.h'
 /**
- * _strlen - function that prints half of a string
+ * puts_half - returns the length of a string
  * @s: string
  * Return: length
  */
-int _strlen(char *s)
+int puts_half(char *s)
 {
 int longi = 0;
-
 while (*s != '\0')
 {
 longi++;
 s++;
 }
-
 return (longi);
 }
